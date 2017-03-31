c54ecbd06c6eb6ff83ae74a21669cc37396390a5
Mariano <mariano.palliser@gmail.com>
2017-03-31 13:48:09 +0200

    añadido kata numeros romanos

---
+++ Numeros romanos/NumerosRomanos/src/test/java/org/foobarspam/NumerosRomanos/AppTest.java
@@ -0,0 +1,22 @@
+package org.foobarspam.NumerosRomanos;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    @Test
+    public void firstTest(){
+    	
+    }
+    
+    
+}
